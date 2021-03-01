b2478136fc64ef3bbda982ae7b593ac2253f1886
CatMan <garfield_lv@163.com>
2021-03-01 18:29:44 +0800

    feat(insertion sort): java insertion sort

---
+++ 01_basic/03_插入排序/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+    public static void main (String[] args) {
+        int[] nums = new int[]{6, 4, 7, 2, 9, 3, 1, 5, 8};
+        int len = nums.length;
+        printArray(nums);
+
+        for (int i=1;i<len;i++){
+            for (int j=i;j>=1;j--){
+                if (nums[j]<nums[j-1]){
+                    moveLeft(nums,j);
+                }
+            }
+            printArray(nums);
+        }
+    }
+
+    public static void printArray( int[] arr) {
+
+        System.out.print("[");
+        for (int i=0; i<arr.length ;i++){
+            System.out.print(arr[i]+",");
+        }
+        System.out.println("]");
+
+
+    }
+
+    private static void moveLeft(int[] arr, int index) {
+
+        int tmp = arr[index];
+        arr[index]=arr[index-1];
+        arr[index-1] = tmp;
+    }
+
+}
