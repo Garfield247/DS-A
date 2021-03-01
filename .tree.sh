00ded3bf14b07bb0e0274af338faadc0f25ca5b9
lvgang <lvgang@shtdtech.com>
2021-03-01 09:27:57 +0800

    feat(first commit): rEADME

---
+++ .tree.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Author:Garfield_lv
+
+f1="./README.md"
+
+echo -e "# Data Structures and Algorithms\n" > $f1
+echo -e "## 目录\n" > $f1
+
+
+function func(){
+    for p in `ls $1`
+        do
+            if [ -d $1"/"$p ]
+                then
+            echo -e "- $1"/"${p}\n" >> $f1
+            func $1"/"$p
+            elif [ -f $1"/"$p ]
+            then
+            echo -e "    [$p]($1"/"$p)\n" >> $f1
+            fi
+        done
+}
+func .
